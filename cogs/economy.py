901f51b3cdf45fb45e2e5d900afd797659759ee9
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-02-23 13:45:57 -0800

    smh

--- cogs/economy.py
+++ cogs/economy.py
@@ -18,7 +18,7 @@ class economy(commands.Cog):
                 users[str(author.id)]["wallet"] = 0
                 users[str(author.id)]["bank"] = 0
                 with open("bank.json", "w") as f:
-                    json.dump(users, f)
+                    json.dump(users, f, indent=4)
 
     @asyncexe()
     def bal_(self, author):
@@ -27,18 +27,20 @@ class economy(commands.Cog):
             if str(author.id) in users:
                 return users[str(author.id)]["wallet"], users[str(author.id)]["bank"]
             else:
-                return False, False
+                return None, None
 
     @commands.command()
     async def bal(self, ctx, user:discord.Member=None):
         wallet, bank = await self.bal_(user or ctx.author)
-        if bank == False:
+        if bank == None:
             return await ctx.send("u don't even have a bank account run `open` to open one")
         await ctx.send(embed=discord.Embed(description=f"Wallet: {wallet}\nBank: {bank}"))
 
     @commands.command()
     async def open(self, ctx):
-        await self.open_(ctx.author)
+        o = await self.open_(ctx.author)
+        if o:
+            return await ctx.send("you already have a bank account")
         await ctx.send(f"Succefully opened bank account {ctx.author.id}")
     
 
